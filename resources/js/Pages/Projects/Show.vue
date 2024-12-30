c9de7f5282a352a49b4ccaa42acb06c76a580a35
mls42 <laspina92@gmail.com>
2024-12-30 16:23:32 +0100

    Fix styling

--- resources/js/Pages/Projects/Show.vue
+++ resources/js/Pages/Projects/Show.vue
@@ -1,6 +1,6 @@
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue';
-import { Head, Link } from '@inertiajs/vue3';
+import { Head } from '@inertiajs/vue3';
 import NavLink from '@/Components/NavLink.vue';
 import { defineProps } from 'vue';
 
