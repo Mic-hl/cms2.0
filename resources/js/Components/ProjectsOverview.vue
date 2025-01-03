906205809ce004f40c4003931eb37c2f845d3807
mls42 <laspina92@gmail.com>
2025-01-03 12:47:17 +0100

    Add a simple dashboard view

---
+++ resources/js/Components/ProjectsOverview.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <div class="projects-overview">
+        <h3>total projects</h3>
+        <span class="projects-number">12</span>
+        <Link
+            :href="route('projects.index', { page: projectsPage })"
+            class="projects-link"
+        >
+            show projects
+        </Link>
+        <Link
+            :href="route('projects.create')"
+            class="project-create-link"
+        >
+            add new project
+        </Link>
+    </div>
+</template>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,6 +1,7 @@
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue';
 import Welcome from '@/Components/Welcome.vue';
+import ProjectsOverview from '@/Components/ProjectsOverview.vue';
 </script>
 
 <template>
@@ -11,7 +12,10 @@ import Welcome from '@/Components/Welcome.vue';
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
+                <div class="dashboard-wrapper overflow-hidden">
+                    <ProjectsOverview
+                        v-if="$page.props.profile.user.isGlobalAdmin"
+                    />
                     <Welcome />
                 </div>
             </div>
